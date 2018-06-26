3f58332de1b9f5141d2139d161c8dc9e93b1c9e3
luigimaia99 <luigimaia99@gmail.com>
2018-06-26 17:15:29 -0300

    Incício da programação de uma árvore.

---
+++ Include/tree.hpp
@@ -0,0 +1,25 @@
+/** @file tree.hpp
+ @author
+   * Luigi Minardi Ferreira Maia
+   * Danilo Raposo Freire Caldas
+ @date 06/26/2018
+ @brief Arquivo de contendo a implementação de uma árvore com implementação em
+ meta-programação (template).
+ @copyright GNU Public License.
+*/
+
+#ifndef TREE_HPP_
+#define TREE_HPP_
+
+namespace chess {
+
+/** @brief
+ *
+ @tparam Item  Tipo do item que será manipulado pelos nós das árvores.
+*/
+template <class Item>
+class Tree {
+};
+
+} // namespace chess
+#endif // TREE_HPP_
