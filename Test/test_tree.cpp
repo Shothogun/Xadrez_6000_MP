6c76e12754c9dda93ad5ecb3fa7c46d097d68d1e
luigimaia99 <luigimaia99@gmail.com>
2018-06-28 05:26:47 -0300

    Nova função que retorna item guardado na árvore.

--- Test/test_tree.cpp
+++ Test/test_tree.cpp
@@ -3,17 +3,23 @@
 #include "catch.hpp"
 #include "tree.hpp"
 
-TEST_CASE("Construtor") {
+TEST_CASE ("Construtor") {
   chess::Tree<int> tree;
-  SECTION("Função size") {
-    CHECK(tree.size() == 0);
+  SECTION ( "Função size") {
+    CHECK (tree.size() == 0);
   }
 }
 
 TEST_CASE("Inserir raiz") {
   chess::Tree<int> tree;
   tree.insertRoot(0);
-  SECTION("Função size") {
-    CHECK(tree.size() == 1);
+  SECTION ("Ver se o tamanho da árvore é 1 e contém o item certo") {
+    CHECK (tree.size() == 1);
+    CHECK (tree.item() == 0);
+  }
+  tree.insertRoot(1);
+  SECTION ("Sobreposição de inserção de raízes deve resultar em uma nova árvore") {
+    CHECK (tree.size() == 1);
+    CHECK (tree.item() == 1);
   }
 }
