33e047432ef6d4a234d4750546f68807f0eca402
luigimaia99 <luigimaia99@gmail.com>
2018-06-29 00:43:57 -0300

    Teste Burro tirado

--- Test/test_tree.cpp
+++ Test/test_tree.cpp
@@ -8,6 +8,7 @@ TEST_CASE ("Construtor") {
   SECTION ( "Árvore vazia sem nenhum nó") {
     CHECK (tree.size() == 0);
     CHECK (tree.isRoot() == false);
+    CHECK (tree.numSons() == -1);
   }
 }
 
@@ -18,23 +19,22 @@ TEST_CASE ("Inserir raiz") {
     CHECK (tree.size() == 1);
     CHECK (tree.item() == 0);
     CHECK (tree.isRoot() == true);
+    CHECK (tree.numSons() == 0);
   }
   tree.insertRoot(1);
   SECTION ("Sobreposição de inserção de raízes deve resultar em uma nova árvore") {
     CHECK (tree.size() == 1);
     CHECK (tree.item() == 1);
+    CHECK (tree.isRoot() == true);
+    CHECK (tree.numSons() == 0);
   }
 }
 
 TEST_CASE ("Inserir filho na raiz") {
   chess::Tree<int> tree;
   tree.insertSon(0);
-  SECTION("Deve haver apenas um item '0' na raiz") {
-    CHECK (tree.size() == 1);
-    CHECK (tree.item() == 0);
-  }
   tree.insertSon(10);
-  SECTION ("Tamnho da árvore deve ser 2 e o item do primeiro filho deve ser 10") {
+  SECTION ("Tamanho da árvore deve ser 2 e o item do primeiro filho deve ser 10") {
     CHECK (tree.size() == 2 );
   }
 }
