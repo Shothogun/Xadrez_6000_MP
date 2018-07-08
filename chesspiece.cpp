2df5987a7c506d3eb24a0f691260c4ac5087955d
HSuper-8 <hevelyn.sthefany@gmail.com>
2018-07-08 19:57:43 -0300

    Relatorios e Comentarios no modulo Peças

--- chesspiece.cpp
+++ chesspiece.cpp
@@ -1,15 +1,25 @@
 #include "../chesspiece.h"
 #include <QPixmap>
 
-
+/** \brief Construtor da Peça
+        A função inicializa a cor, o tipo e o peso das peças. 
+        O vetor com os centros das regiões também é criado.
+        As o objeto com sua imagem é colocada no tabuleiro.
+        \param Cor, tipo e peso da peça, cenário para peça (parent)
+*/
 Piece::Piece(PieceColor c, PieceType p, int weight, QGraphicsItem* parent)
 {
     this->color = c;
     this->type = p;
     this->weight = weight;
+    this->CentersRegions();
     this->setImage(c,p);
 }
 
+/** \brief Coloca a imagem da peça no tabuleiro.
+        A Função colcoa as peças no cenário do tabuleiro
+        juntamente com sua imagem.
+*/
 void Piece::setImage(PieceColor c, PieceType type)
 {
     //-------------------- White pieces --------------------//
@@ -103,6 +113,10 @@ void Piece::setImage(PieceColor c, PieceType type)
 
   }
 
+/** \brief Emite som ao jogador clicar no mouse
+        ao clicar a peça com o mouse, emite som.
+    \param click do mouse
+*/
 void Piece::mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent)
 {
     if(mouseEvent->button() == Qt::LeftButton)
@@ -113,6 +127,11 @@ void Piece::mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent)
     }
 }
 
+/** \brief Realiza movimento da peça ao movimentar com ela 
+        pelo mouse.
+        O objeto peça se moviemento junto com o movimento do mouse.
+    \param Movimento do mouse
+*/
 void Piece::mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent)
 {
     // Corrects mouse position while move
@@ -124,34 +143,53 @@ void Piece::mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent)
     }
 }
 
+/** \brief Realiza movimento da peça ao centro da região escolhida pelo jogador.
+        Ao soltar o mouse, a função verifica a posição atual, calcula
+        qual centro de região mais próxima e colcoa a peça nessa região.
+        Caso a região seja o topo do tabuleiro, a peça é promovida.
+    \param Ação de soltar o mouse
+*/
 void Piece::mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent)
 {
     QMediaPlayer* sound = new QMediaPlayer();
     sound->setMedia(QUrl("qrc:/sound_test/BIO03.WAV"));
     sound->play();
-    //int idx = this->FoundCenterRegion(mouseEvent->pos());
-    //this->setOffset(this->centers[idx]);
+    int idx = this->FoundCenterRegion(mouseEvent->pos());
+    this->setOffset(this->centers[idx]);
     this->promotion(mouseEvent->pos());
 }
 
-void Piece::CentersRegions(){
-    int x = 0, y = 0;
-    for(int i = 0; i < 8; i ++){
-        for(int j = 0; j < 8; j ++){
-            this->centers.push_back(QPointF(x+20, y+20));
-            y += 40;
+/** \brief Cria vetor com os centros das regiões.
+        Calcula o centro de cada quadrado do tabuleiro e guarda
+        seu centro num vetor.
+*/
+void Piece::CentersRegions()
+{
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j < 8; j++)
+        {
+            this->centers.insert(this->centers.end() ,QPointF(25+50*i, 30+50*j));
         }
-        x += 40;
     }
 }
 
-int Piece::FoundCenterRegion(QPointF point){
+/** \brief Encontra o centro mais proximo da posição dada.
+        A função calcula a distancia euclidiana do ponto dado
+        com cada centro do tabuleiro e devolve o indice da posição
+        do centro mais proximo à esse ponto no vetor.
+    \param Posição da peça ao soltar o mouse
+*/
+int Piece::FoundCenterRegion(QPointF point)
+{
     int idxCenter;
-    float distance, smaller = 0.0;
-    for(int i = 0; i < 64; i++){
+    float distance, smaller = 1000;
+    for(int i = 0; i < 64; i++)
+    {
         QPointF pointCenter = this->centers[i];
         distance = std::sqrt( std::pow(point.x() - pointCenter.x(), 2) + std::pow(point.y() - pointCenter.y(), 2));
-        if(distance < smaller){
+        if(distance < smaller)
+        {
             smaller = distance;
             idxCenter = i;
         }
@@ -159,65 +197,31 @@ int Piece::FoundCenterRegion(QPointF point){
     return idxCenter;
 }
 
-void Piece::promotion(QPointF point){
+/** \brief Promoção da Peça.
+        A função verifica se o ponto dado (posição atual da peça no tabuleiro)
+        é o fim do tabuleiro e entao verifica qual sua cor para
+        então fazer a promoção para rainha.
+    \param Posição atual da peça.
+*/
+void Piece::promotion(QPointF point)
+{
     QPixmap image;
-    if(this->color == PieceColor::White && point.y() == 800){
+
+    if(this->color == PieceColor::White && point.y() < 50)
+    {
         this->type = PieceType::queen;
         this->weight = 9;
         image = QPixmap(":/chessboard/chess_icons/WQ.png");
+        image = image.scaled( QSize(40,40),  Qt::IgnoreAspectRatio, Qt::FastTransformation);
+        setPixmap(QPixmap(image));
     }
-    if(this->color == PieceColor::Black && point.y() == 0){
+
+    if(this->color == PieceColor::Black && point.y() > 380)
+    {
         this->type = PieceType::queen;
         this->weight = 9;
         image = QPixmap(":/chessboard/chess_icons/BQ.png");
-    }
-    image = image.scaled( QSize(40,40),  Qt::IgnoreAspectRatio, Qt::FastTransformation);
-    setPixmap(QPixmap(image));
-}
-
-bool Piece::checkPawnMove(int fromr, int fromc, int tor, int toc, bool first, bool diagonal){
-    bool ok = true;
-    if(tor - fromr < 0) ok = false;
-    if(first && (!diagonal) && tor - fromr > 100) ok = false;
-    if(!first && (!diagonal) && tor - fromr > 50) ok = false);
-    if(diagonal && ( tor - fromr > 50 || toc - fromc > 50) ) ok = false;
-    return ok;
-}
-
-bool Piece::checkRookMove(int fromr, int fromc, int tor, int toc){
-    if( (tor != fromr) && (toc != fromc)) return false;
-    else return true;
-}
-
-bool Piece::checkKnightMove(int fromr, int fromc, int tor, int toc){
-    if((abs(tor - fromr) != 100 && abs(toc - fromc) != 50) || (abs(tor - fromr) != 50 && abs(toc - fromc) != 100))
-        return false;
-    else return true;
-}
-
-bool Piece::checkBishopMove(int fromr, int fromc, int tor, int toc){
-    if(tor == fromr || toc == fromc) return false;
-    else return true;
-}
-
-bool Piece::checkKingMove(int fromr, int fromc, int tor, int toc){
-    if(abs(tor - fromr) != abs(toc - fromc) ) return false;
-    else return true;
-}
-
-bool Piece::checkMove(int fromr, int fromc, int tor, int toc){
-    if(tor > 800 || tor < 0 || toc > 800 || toc < 0) return = false
-        return false;
-    else{
-        if(this->type == PieceType::pawn) 
-            return checkPawnMove(int fromr, int fromc, int tor, int toc, this->first);
-        if(this->type == PieceType::rook)
-            return checkRookMove(int fromr, int fromc, int tor, int toc);
-        if(this->type == PieceType::knight)
-            return checkKnightMove(int fromr, int fromc, int tor, int toc);
-        if(this->type == PieceType::bishop)
-            return checkBishopMove(int fromr, int fromc, int tor, int toc);
-        if(this->type == PieceType::king)
-            return checkKingMove(int fromr, int fromc, int tor, int toc);
+        image = image.scaled( QSize(40,40),  Qt::IgnoreAspectRatio, Qt::FastTransformation);
+        setPixmap(QPixmap(image));
     }
 }
--- chesspiece.h
+++ chesspiece.h
@@ -11,39 +11,39 @@
 #include <QMediaPlayer>
 #include <QObject>
 
+/*! Classe enumerada com as possíveis cores das peças */
 enum class PieceColor { White, Black };
+/*! Classe enumerada com os possíveis tipos das peças */
 enum class PieceType { pawn, knight, bishop, rook, queen, king };
 
+/*! Classe da Peça */
 class Piece: public QGraphicsPixmapItem
 {
 public:
 
-  PieceColor color;
-  PieceType type;
-	int weight;
-	QPointF position;
-	std::list<std::string> lost_Pieces;
-  std::vector<QPointF> centers;
-  bool first; // for Pawn
+  PieceColor color; //!< Cor da peça
+  PieceType type; //!< tipo da peça
+	int weight; //!< Peso da peça
+	QPointF position; //!< Posição atual da peça
+  std::vector<QPointF> centers; //!< Vetor que guarda os centros das regiões
+  bool first; // Variável para o peão, representa se é a primeira jogada ou não
+  bool diagonal //!< Variável para o peão, representa se pode se movimentar à diagonal
+	
+  Piece(PieceColor c, PieceType p, int weight, QGraphicsItem* parent = 0); //!< Construtor da peça
 
-	// Como usar:
-    // Piece piece(Color::White, Piece::pawn, number);
-  Piece(PieceColor c, PieceType p, int weight, QGraphicsItem* parent = 0);
-
-  void setImage(PieceColor c, PieceType type);
-  void mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent);
-  void mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent);
-  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event);
-  void promotion(QPointF point);
-  void CentersRegions();
-  int FoundCenterRegion(QPointF point);
-  bool checkPawnMove(int fromr, int fromc, int tor, int toc);
-  bool checkRookMove(int fromr, int fromc, int tor, int toc);
-  bool checkKnightMove(int fromr, int fromc, int tor, int toc);
-  bool checkKnightMove(int fromr, int fromc, int tor, int toc);
-  bool checkBishopMove(int fromr, int fromc, int tor, int toc);
-  bool checkKingMove(int fromr, int fromc, int tor, int toc);
-  bool checkMove(int fromr, int fromc, int tor, int toc);
+  void setImage(PieceColor c, PieceType type); //!< Coloca a imagem da peça no tabuleiro
+  void mousePressEvent(QGraphicsSceneMouseEvent* mouseEvent); //!< Cuida das ações ao clicar um objeto com o mouse
+  void mouseMoveEvent(QGraphicsSceneMouseEvent* mouseEvent); //!< Cuida das ações movimentar um objeto com o mouse
+  void mouseReleaseEvent(QGraphicsSceneMouseEvent* event); //!< Cuida das ações soltar um objeto com o mouse
+  void promotion(QPointF point); //!< Promoção da peça
+  void CentersRegions(); //!< Cria vetor com os centros das regiões
+  int FoundCenterRegion(QPointF point); //!< Encontra região mais próxima da posição dada
+  bool checkPawnMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento do peão
+  bool checkRookMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento da torre
+  bool checkKnightMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento do cavalo
+  bool checkBishopMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento do bispo
+  bool checkKingMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento do rei
+  bool checkMove(int fromr, int fromc, int tor, int toc); //!< checa o movimento das peças
 };
 
 
